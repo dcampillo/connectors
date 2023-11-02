0b90480db2fa6c622f988e3798ed3434a9b63976
Chenhui Wang <54903978+wangch079@users.noreply.github.com>
2023-11-02 13:53:50 +0800

    Break JobExecutionSerivce to ContentSyncJobExecutionService and AccessControlSyncJobExecutionService (#1840)

--- connectors/services/access_control_sync_job_execution.py
+++ connectors/services/access_control_sync_job_execution.py
@@ -3,7 +3,6 @@
 # or more contributor license agreements. Licensed under the Elastic License 2.0;
 # you may not use this file except in compliance with the Elastic License 2.0.
 #
-
 from connectors.es.client import License
 from connectors.es.index import DocumentNotFoundError
 from connectors.es.license import requires_platinum_license
@@ -20,81 +19,26 @@ from connectors.source import get_source_klass
 from connectors.sync_job_runner import SyncJobRunner
 from connectors.utils import ConcurrentTasks
 
-DEFAULT_MAX_CONCURRENT_CONTENT_SYNCS = 1
 DEFAULT_MAX_CONCURRENT_ACCESS_CONTROL_SYNCS = 1
 
 
-def load_max_concurrent_access_control_syncs(config):
-    return config.get(
-        "max_concurrent_access_control_syncs",
-        DEFAULT_MAX_CONCURRENT_ACCESS_CONTROL_SYNCS,
-    )
-
-
-def load_max_concurrent_content_syncs(config):
-    max_concurrent_content_syncs = config.get("max_concurrent_content_syncs")
-
-    if max_concurrent_content_syncs is not None:
-        return max_concurrent_content_syncs
-
-    logger.warning(
-        "'max_concurrent_syncs' is deprecated. Use 'max_concurrent_content_syncs' in 'config.yml'."
-    )
-
-    # keep for backwards compatibility
-    return config.get("max_concurrent_syncs", DEFAULT_MAX_CONCURRENT_CONTENT_SYNCS)
-
-
-class JobExecutionService(BaseService):
-    name = "execute"
+class AccessControlSyncJobExecutionService(BaseService):
+    name = "sync_access_control"
 
     def __init__(self, config):
         super().__init__(config)
         self.idling = self.service_config["idling"]
-        self.max_concurrent_content_syncs = load_max_concurrent_content_syncs(
-            self.service_config
-        )
-        self.max_concurrent_access_control_syncs = (
-            load_max_concurrent_access_control_syncs(self.service_config)
-        )
         self.source_list = config["sources"]
-        self.content_syncs = None
+        self.max_concurrent_access_control_syncs = self.service_config.get(
+            "max_concurrent_access_control_syncs",
+            DEFAULT_MAX_CONCURRENT_ACCESS_CONTROL_SYNCS,
+        )
         self.access_control_syncs = None
 
     def stop(self):
         super().stop()
-        if self.content_syncs is not None:
-            self.content_syncs.cancel()
         if self.access_control_syncs is not None:
             self.access_control_syncs.cancel()
-
-    async def _content_sync(self, sync_job, connector, source_klass):
-        if connector.last_sync_status == JobStatus.IN_PROGRESS:
-            sync_job.log_debug("Connector is still syncing content, skip the job...")
-            return
-
-        sync_job_runner = SyncJobRunner(
-            source_klass=source_klass,
-            sync_job=sync_job,
-            connector=connector,
-            es_config=self._override_es_config(connector),
-        )
-        await self.content_syncs.put(sync_job_runner.execute)
-
-    async def _access_control_sync(self, sync_job, connector, source_klass):
-        if connector.last_access_control_sync_status == JobStatus.IN_PROGRESS:
-            sync_job.log_debug(
-                "Connector is still syncing access control, skip the job..."
-            )
-            return
-
-        sync_job_runner = SyncJobRunner(
-            source_klass=source_klass,
-            sync_job=sync_job,
-            connector=connector,
-            es_config=self._override_es_config(connector),
-        )
-        await self.access_control_syncs.put(sync_job_runner.execute)
 
     async def _sync(self, sync_job):
         if sync_job.service_type not in self.source_list:
@@ -122,21 +66,19 @@ class JobExecutionService(BaseService):
                 )
                 return
 
-        job_type = sync_job.job_type
-
-        if job_type in [JobType.FULL, JobType.INCREMENTAL]:
-            await self._content_sync(sync_job, connector, source_klass)
-
-        elif (
-            job_type == JobType.ACCESS_CONTROL
-            and connector.features.document_level_security_enabled()
-        ):
-            await self._access_control_sync(sync_job, connector, source_klass)
-
-        else:
-            sync_job.log_error(
-                f"Unsupported job type '{job_type}'. Skipping sync job execution..."
+        if connector.last_access_control_sync_status == JobStatus.IN_PROGRESS:
+            sync_job.log_debug(
+                "Connector is still syncing access control, skip the job..."
             )
+            return
+
+        sync_job_runner = SyncJobRunner(
+            source_klass=source_klass,
+            sync_job=sync_job,
+            connector=connector,
+            es_config=self._override_es_config(connector),
+        )
+        await self.access_control_syncs.put(sync_job_runner.execute)
 
     async def _run(self):
         self.connector_index = ConnectorIndex(self.es_config)
@@ -145,30 +87,29 @@ class JobExecutionService(BaseService):
         native_service_types = self.config.get("native_service_types", []) or []
         if len(native_service_types) > 0:
             logger.debug(
-                f"Native support for job execution for {', '.join(native_service_types)}"
+                f"Native support for access control sync job execution for {', '.join(native_service_types)}"
             )
         else:
-            logger.debug("No native service types configured for job execution")
+            logger.debug(
+                "No native service types configured for access control sync job execution"
+            )
 
         connector_ids = list(self.connectors.keys())
 
         logger.info(
-            f"Job Execution Service started, listening to events from {self.es_config['host']}"
+            f"Access control sync job execution service started, listening to events from {self.es_config['host']}"
         )
 
         try:
             while self.running:
                 # creating pools of tasks for every round
-                self.content_syncs = ConcurrentTasks(
-                    max_concurrency=self.max_concurrent_content_syncs
-                )
                 self.access_control_syncs = ConcurrentTasks(
                     max_concurrency=self.max_concurrent_access_control_syncs
                 )
 
                 try:
                     logger.debug(
-                        f"Polling every {self.idling} seconds for Job Execution"
+                        f"Polling every {self.idling} seconds for access control sync job execution"
                     )
                     supported_connector_ids = [
                         connector.id
@@ -185,21 +126,15 @@ class JobExecutionService(BaseService):
                     else:
                         async for sync_job in self.sync_job_index.pending_jobs(
                             connector_ids=supported_connector_ids,
-                            job_types=[
-                                JobType.FULL.value,
-                                JobType.INCREMENTAL.value,
-                                JobType.ACCESS_CONTROL.value,
-                            ],
+                            job_types=JobType.ACCESS_CONTROL.value,
                         ):
                             await self._sync(sync_job)
                 except Exception as e:
                     logger.critical(e, exc_info=True)
                     self.raise_if_spurious(e)
                 finally:
-                    await self.content_syncs.join()
                     await self.access_control_syncs.join()
 
-                self.content_syncs = None
                 self.access_control_syncs = None
 
                 # Immediately break instead of sleeping
